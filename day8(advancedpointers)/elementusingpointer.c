a790dd02875e001e87902b8105f672681f7d9951
pranjal bangera <pranjalbangera19@gmail.com>
2026-06-05 22:26:44 +0530

    Initial commit

--- day8(advancedpointers)/elementusingpointer.c
+++ day8(advancedpointers)/elementusingpointer.c
@@ -2,10 +2,15 @@
     #include<stdio.h>
     int main()
     {
-        int max=*ptr;
-        for(int i=1;i<n;i++)
-        {
-            if(*(ptr+i)>max)
-            max=*(ptr+i);
-        }
+        int arr[] = {5, 2, 9, 1, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int *ptr = arr;
+    int max = *ptr;
+    for (int i = 1; i < n; i++)
+    {
+        if (*(ptr + i) > max)
+            max = *(ptr + i);
     }
+    printf("Max element: %d\n", max);
+    return 0;
+}
